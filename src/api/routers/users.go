2a660b7de33542d9987a2d7f70cab24e4d6515df
Hamed Naeemaei <h.naimaei@gmail.com>
2023-02-04 01:04:28 +0330

    Add IP rate limiter to send-otp endpoint

--- src/api/routers/users.go
+++ src/api/routers/users.go
@@ -3,11 +3,12 @@ package routers
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/naeemaei/golang-clean-web-api/api/handlers"
+	"github.com/naeemaei/golang-clean-web-api/api/middlewares"
 	"github.com/naeemaei/golang-clean-web-api/config"
 )
 
 func User(router *gin.RouterGroup, cfg *config.Config) {
 	h := handlers.NewUsersHandler(cfg)
 
-	router.POST("/send-otp", h.SendOtp)
+	router.POST("/send-otp", middlewares.OtpLimiter(cfg), h.SendOtp)
 }
---
+++ src/pkg/limiter/ip_limiter.go
@@ -0,0 +1,56 @@
+package limiter
+
+import (
+	"sync"
+
+	"golang.org/x/time/rate"
+)
+
+// IPRateLimiter .
+type IPRateLimiter struct {
+	ips map[string]*rate.Limiter
+	mu  *sync.RWMutex
+	r   rate.Limit
+	b   int
+}
+
+// NewIPRateLimiter .
+func NewIPRateLimiter(r rate.Limit, b int) *IPRateLimiter {
+	i := &IPRateLimiter{
+		ips: make(map[string]*rate.Limiter),
+		mu:  &sync.RWMutex{},
+		r:   r,
+		b:   b,
+	}
+
+	return i
+}
+
+// AddIP creates a new rate limiter and adds it to the ips map,
+// using the IP address as the key
+func (i *IPRateLimiter) AddIP(ip string) *rate.Limiter {
+	i.mu.Lock()
+	defer i.mu.Unlock()
+
+	limiter := rate.NewLimiter(i.r, i.b)
+
+	i.ips[ip] = limiter
+
+	return limiter
+}
+
+// GetLimiter returns the rate limiter for the provided IP address if it exists.
+// Otherwise calls AddIP to add IP address to the map
+func (i *IPRateLimiter) GetLimiter(ip string) *rate.Limiter {
+	i.mu.Lock()
+	limiter, exists := i.ips[ip]
+
+	if !exists {
+		i.mu.Unlock()
+		return i.AddIP(ip)
+	}
+
+	i.mu.Unlock()
+
+	return limiter
+}
