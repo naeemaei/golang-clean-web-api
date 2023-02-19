566149607ca6eafa5bfb2fe2e361b982f49168e5
Hamed Naeemaei <h.naimaei@gmail.com>
2023-02-20 02:00:49 +0330

    nil filter and nil sort bug fixed

--- src/services/base_service.go
+++ src/services/base_service.go
@@ -180,38 +180,40 @@ func getQuery[T any](filter *dto.DynamicFilter) string {
 	typeT := reflect.TypeOf(*t)
 	query := make([]string, 0)
 	query = append(query, "deleted_by is null")
-	for name, filter := range filter.Filter {
-		fld, ok := typeT.FieldByName(name)
-		if ok {
-			fld.Name = common.ToSnakeCase(fld.Name)
-			switch filter.Type {
-			case "contains":
-				query = append(query, fmt.Sprintf("%s Ilike '%%%s%%'", fld.Name, filter.From))
-			case "notContains":
-				query = append(query, fmt.Sprintf("%s not Ilike '%%%s%%'", fld.Name, filter.From))
-			case "startsWith":
-				query = append(query, fmt.Sprintf("%s Ilike '%s%%'", fld.Name, filter.From))
-			case "endsWith":
-				query = append(query, fmt.Sprintf("%s Ilike '%%%s'", fld.Name, filter.From))
-			case "equals":
-				query = append(query, fmt.Sprintf("%s = '%s'", fld.Name, filter.From))
-			case "notEqual":
-				query = append(query, fmt.Sprintf("%s != '%s'", fld.Name, filter.From))
-			case "lessThan":
-				query = append(query, fmt.Sprintf("%s < %s", fld.Name, filter.From))
-			case "lessThanOrEqual":
-				query = append(query, fmt.Sprintf("%s <= %s", fld.Name, filter.From))
-			case "greaterThan":
-				query = append(query, fmt.Sprintf("%s > %s", fld.Name, filter.From))
-			case "greaterThanOrEqual":
-				query = append(query, fmt.Sprintf("%s >= %s", fld.Name, filter.From))
-			case "inRange":
-				if fld.Type.Kind() == reflect.String {
-					query = append(query, fmt.Sprintf("%s >= '%s'", fld.Name, filter.From))
-					query = append(query, fmt.Sprintf("%s <= '%s'", fld.Name, filter.To))
-				} else {
+	if filter.Filter != nil {
+		for name, filter := range filter.Filter {
+			fld, ok := typeT.FieldByName(name)
+			if ok {
+				fld.Name = common.ToSnakeCase(fld.Name)
+				switch filter.Type {
+				case "contains":
+					query = append(query, fmt.Sprintf("%s Ilike '%%%s%%'", fld.Name, filter.From))
+				case "notContains":
+					query = append(query, fmt.Sprintf("%s not Ilike '%%%s%%'", fld.Name, filter.From))
+				case "startsWith":
+					query = append(query, fmt.Sprintf("%s Ilike '%s%%'", fld.Name, filter.From))
+				case "endsWith":
+					query = append(query, fmt.Sprintf("%s Ilike '%%%s'", fld.Name, filter.From))
+				case "equals":
+					query = append(query, fmt.Sprintf("%s = '%s'", fld.Name, filter.From))
+				case "notEqual":
+					query = append(query, fmt.Sprintf("%s != '%s'", fld.Name, filter.From))
+				case "lessThan":
+					query = append(query, fmt.Sprintf("%s < %s", fld.Name, filter.From))
+				case "lessThanOrEqual":
+					query = append(query, fmt.Sprintf("%s <= %s", fld.Name, filter.From))
+				case "greaterThan":
+					query = append(query, fmt.Sprintf("%s > %s", fld.Name, filter.From))
+				case "greaterThanOrEqual":
 					query = append(query, fmt.Sprintf("%s >= %s", fld.Name, filter.From))
-					query = append(query, fmt.Sprintf("%s <= %s", fld.Name, filter.To))
+				case "inRange":
+					if fld.Type.Kind() == reflect.String {
+						query = append(query, fmt.Sprintf("%s >= '%s'", fld.Name, filter.From))
+						query = append(query, fmt.Sprintf("%s <= '%s'", fld.Name, filter.To))
+					} else {
+						query = append(query, fmt.Sprintf("%s >= %s", fld.Name, filter.From))
+						query = append(query, fmt.Sprintf("%s <= %s", fld.Name, filter.To))
+					}
 				}
 			}
 		}
@@ -223,12 +225,13 @@ func getSort[T any](filter *dto.DynamicFilter) string {
 	t := new(T)
 	typeT := reflect.TypeOf(*t)
 	sort := make([]string, 0)
-
-	for _, tp := range *filter.Sort {
-		fld, ok := typeT.FieldByName(tp.ColId)
-		fld.Name = common.ToSnakeCase(fld.Name)
-		if ok {
-			sort = append(sort, fmt.Sprintf("%s %s", fld.Name, tp.Sort))
+	if filter.Sort != nil {
+		for _, tp := range *filter.Sort {
+			fld, ok := typeT.FieldByName(tp.ColId)
+			fld.Name = common.ToSnakeCase(fld.Name)
+			if ok {
+				sort = append(sort, fmt.Sprintf("%s %s", fld.Name, tp.Sort))
+			}
 		}
 	}
 	return strings.Join(sort, ", ")
