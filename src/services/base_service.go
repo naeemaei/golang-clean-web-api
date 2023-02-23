11740bdfd32c582523801ec5e3e8f8f843cbcb91
Hamed Naeemaei <h.naimaei@gmail.com>
2023-02-24 00:36:59 +0330

    Generic dynamic filter and pagination implemented

--- src/services/base_service.go
+++ src/services/base_service.go
@@ -3,20 +3,31 @@ package services
 import (
 	"context"
 	"database/sql"
+	"fmt"
+	"math"
+	"reflect"
+	"strings"
 	"time"
 
+	"github.com/naeemaei/golang-clean-web-api/api/dto"
 	"github.com/naeemaei/golang-clean-web-api/common"
 	"github.com/naeemaei/golang-clean-web-api/config"
 	"github.com/naeemaei/golang-clean-web-api/constants"
 	"github.com/naeemaei/golang-clean-web-api/data/db"
+	"github.com/naeemaei/golang-clean-web-api/data/models"
 	"github.com/naeemaei/golang-clean-web-api/pkg/logging"
 	"github.com/naeemaei/golang-clean-web-api/pkg/service_errors"
 	"gorm.io/gorm"
 )
 
+type preload struct {
+	Base  string
+	Inner []preload
+}
 type BaseService[T any, Tc any, Tu any, Tr any] struct {
 	Database *gorm.DB
 	Logger   logging.Logger
+	Preloads []preload
 }
 
 func NewBaseService[T any, Tc any, Tu any, Tr any](cfg *config.Config) *BaseService[T, Tc, Tu, Tr] {
@@ -39,19 +50,24 @@ func (s *BaseService[T, Tc, Tu, Tr]) Create(ctx context.Context, req *Tc) (*Tr, 
 		return nil, err
 	}
 	tx.Commit()
-	return common.TypeConverter[Tr](model)
+	bm, _ := common.TypeConverter[models.BaseModel](model)
+	return s.GetById(ctx, bm.Id)
 }
 
 func (s *BaseService[T, Tc, Tu, Tr]) Update(ctx context.Context, id int, req *Tu) (*Tr, error) {
 
 	updateMap, _ := common.TypeConverter[map[string]interface{}](req)
-	(*updateMap)["modified_by"] = &sql.NullInt64{Int64: int64(ctx.Value(constants.UserIdKey).(float64)), Valid: true}
-	(*updateMap)["modified_at"] = sql.NullTime{Valid: true, Time: time.Now().UTC()}
+	snakeMap := map[string]interface{}{}
+	for k, v := range *updateMap {
+		snakeMap[common.ToSnakeCase(k)] = v
+	}
+	snakeMap["modified_by"] = &sql.NullInt64{Int64: int64(ctx.Value(constants.UserIdKey).(float64)), Valid: true}
+	snakeMap["modified_at"] = sql.NullTime{Valid: true, Time: time.Now().UTC()}
 	model := new(T)
 	tx := s.Database.WithContext(ctx).Begin()
 	if err := tx.Model(model).
 		Where("id = ? and deleted_by is null", id).
-		Updates(*updateMap).
+		Updates(snakeMap).
 		Error; err != nil {
 		tx.Rollback()
 		s.Logger.Error(logging.Postgres, logging.Update, err.Error(), nil)
@@ -71,8 +87,6 @@ func (s *BaseService[T, Tc, Tu, Tr]) Delete(ctx context.Context, id int) error {
 		"deleted_by": &sql.NullInt64{Int64: int64(ctx.Value(constants.UserIdKey).(float64)), Valid: true},
 		"deleted_at": sql.NullTime{Valid: true, Time: time.Now().UTC()},
 	}
-	deleteMap["modified_by"] = &sql.NullInt64{Int64: int64(ctx.Value(constants.UserIdKey).(float64)), Valid: true}
-	deleteMap["modified_at"] = sql.NullTime{Valid: true, Time: time.Now().UTC()}
 
 	if ctx.Value(constants.UserIdKey) == nil {
 		return &service_errors.ServiceError{EndUserMessage: service_errors.PermissionDenied}
@@ -100,4 +114,137 @@ func (s *BaseService[T, Tc, Tu, Tr]) GetById(ctx context.Context, id int) (*Tr, 
 		return nil, err
 	}
 	return common.TypeConverter[Tr](model)
+}
+
+func (s *BaseService[T, Tc, Tu, Tr]) GetByFilter(ctx context.Context, req *dto.PaginationInputWithFilter) (*dto.PagedList[Tr], error) {
+	return Paginate[T, Tr](req, s.Preloads, s.Database)
+}
+
+func NewPagedList[T any](items *[]T, count int64, pageNumber int, pageSize int64) *dto.PagedList[T] {
+	pl := &dto.PagedList[T]{
+		PageNumber: pageNumber,
+		TotalRows:  count,
+		Items:      items,
+	}
+	pl.TotalPages = int(math.Ceil(float64(count) / float64(pageSize)))
+	pl.HasNextPage = pl.PageNumber < pl.TotalPages
+	pl.HasPreviousPage = pl.PageNumber > 1
+
+	return pl
+}
+
+// Paginate
+func Paginate[T any, Tr any](pagination *dto.PaginationInputWithFilter, preloads []preload, db *gorm.DB) (*dto.PagedList[Tr], error) {
+	model := new(T)
+	var items *[]T
+	var rItems *[]Tr
+	db = Preload(db, preloads)
+	query := getQuery[T](&pagination.DynamicFilter)
+	sort := getSort[T](&pagination.DynamicFilter)
+
+	var totalRows int64 = 0
+
+	db.
+		Model(model).
+		Where(query).
+		Count(&totalRows)
+
+	err := db.
+		Where(query).
+		Offset(pagination.GetOffset()).
+		Limit(pagination.GetPageSize()).
+		Order(sort).
+		Find(&items).
+		Error
+
+	if err != nil {
+		return nil, err
+	}
+	rItems, err = common.TypeConverter[[]Tr](items)
+	if err != nil {
+		return nil, err
+	}
+	return NewPagedList(rItems, totalRows, pagination.PageNumber, int64(pagination.PageSize)), err
+
+}
+
+// getQuery
+func getQuery[T any](filter *dto.DynamicFilter) string {
+	t := new(T)
+	typeT := reflect.TypeOf(*t)
+	query := make([]string, 0)
+	query = append(query, "deleted_by is null")
+	if filter.Filter != nil {
+		for name, filter := range filter.Filter {
+			fld, ok := typeT.FieldByName(name)
+			if ok {
+				fld.Name = common.ToSnakeCase(fld.Name)
+				switch filter.Type {
+				case "contains":
+					query = append(query, fmt.Sprintf("%s ILike '%%%s%%'", fld.Name, filter.From))
+				case "notContains":
+					query = append(query, fmt.Sprintf("%s not ILike '%%%s%%'", fld.Name, filter.From))
+				case "startsWith":
+					query = append(query, fmt.Sprintf("%s ILike '%s%%'", fld.Name, filter.From))
+				case "endsWith":
+					query = append(query, fmt.Sprintf("%s ILike '%%%s'", fld.Name, filter.From))
+				case "equals":
+					query = append(query, fmt.Sprintf("%s = '%s'", fld.Name, filter.From))
+				case "notEqual":
+					query = append(query, fmt.Sprintf("%s != '%s'", fld.Name, filter.From))
+				case "lessThan":
+					query = append(query, fmt.Sprintf("%s < %s", fld.Name, filter.From))
+				case "lessThanOrEqual":
+					query = append(query, fmt.Sprintf("%s <= %s", fld.Name, filter.From))
+				case "greaterThan":
+					query = append(query, fmt.Sprintf("%s > %s", fld.Name, filter.From))
+				case "greaterThanOrEqual":
+					query = append(query, fmt.Sprintf("%s >= %s", fld.Name, filter.From))
+				case "inRange":
+					if fld.Type.Kind() == reflect.String {
+						query = append(query, fmt.Sprintf("%s >= '%s'", fld.Name, filter.From))
+						query = append(query, fmt.Sprintf("%s <= '%s'", fld.Name, filter.To))
+					} else {
+						query = append(query, fmt.Sprintf("%s >= %s", fld.Name, filter.From))
+						query = append(query, fmt.Sprintf("%s <= %s", fld.Name, filter.To))
+					}
+				}
+			}
+		}
+	}
+	return strings.Join(query, " AND ")
+}
+
+// getSort
+func getSort[T any](filter *dto.DynamicFilter) string {
+	t := new(T)
+	typeT := reflect.TypeOf(*t)
+	sort := make([]string, 0)
+	if filter.Sort != nil {
+		for _, tp := range *filter.Sort {
+			fld, ok := typeT.FieldByName(tp.ColId)
+			if ok && (tp.Sort == "asc" || tp.Sort == "desc") {
+				fld.Name = common.ToSnakeCase(fld.Name)
+				sort = append(sort, fmt.Sprintf("%s %s", fld.Name, tp.Sort))
+			}
+		}
+	}
+	return strings.Join(sort, ", ")
+}
+
+// Preload
+func Preload(db *gorm.DB, preloads []preload) *gorm.DB {
+	for _, item := range preloads {
+		if item.Base != "" {
+			if item.Inner != nil {
+				inner := func(db *gorm.DB) *gorm.DB {
+					return Preload(db, item.Inner)
+				}
+				db = db.Preload(item.Base, inner)
+			} else {
+				db = db.Preload(item.Base)
+			}
+		}
+	}
+	return db
 }
