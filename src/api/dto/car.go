45e2c01448e4af8384168f38e3c825055e132a14
Hamed Naeemaei <h.naimaei@gmail.com>
2023-03-06 00:53:10 +0330

    Car type and gearbox dto types added

---
+++ src/api/dto/car.go
@@ -0,0 +1,27 @@
+package dto
+
+type CreateCarTypeRequest struct {
+	Name string `json:"name" binding:"required,alpha,min=3,max=15"`
+}
+
+type UpdateCarTypeRequest struct {
+	Name string `json:"name" binding:"required,alpha,min=3,max=15"`
+}
+
+type CarTypeResponse struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+type CreateGearboxRequest struct {
+	Name string `json:"name" binding:"required,alpha,min=3,max=15"`
+}
+
+type UpdateGearboxRequest struct {
+	Name string `json:"name" binding:"required,alpha,min=3,max=15"`
+}
+
+type GearboxResponse struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+}
