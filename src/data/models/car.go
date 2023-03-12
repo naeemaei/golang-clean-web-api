0096c8f8caf378d4ae98b1a3243e04050703ef37
Hamed Naeemaei <h.naimaei@gmail.com>
2023-03-13 02:03:21 +0330

    Set yearId and CarId as unique index

--- src/data/models/car.go
+++ src/data/models/car.go
@@ -48,10 +48,10 @@ type CarModelColor struct {
 
 type CarModelYear struct {
 	BaseModel
-	CarModel               CarModel `gorm:"foreignKey:CarModelId;constraint:OnUpdate:NO ACTION;OnDelete:NO ACTION"`
-	CarModelId             int
+	CarModel               CarModel    `gorm:"foreignKey:CarModelId;constraint:OnUpdate:NO ACTION;OnDelete:NO ACTION"`
+	CarModelId             int         `gorm:"uniqueIndex:idx_CarModelId_PersianYearId"`
 	PersianYear            PersianYear `gorm:"foreignKey:PersianYearId;constraint:OnUpdate:NO ACTION;OnDelete:NO ACTION"`
-	PersianYearId          int
+	PersianYearId          int         `gorm:"uniqueIndex:idx_CarModelId_PersianYearId"`
 	CarModelPriceHistories []CarModelPriceHistory
 }
 
