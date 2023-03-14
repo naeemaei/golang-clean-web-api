87a4dbcbb32cbee279d4462424e6a1b5747239bc
Hamed Naeemaei <h.naimaei@gmail.com>
2023-03-14 23:34:43 +0330

    Car image gallery route and static routes added

--- src/api/api.go
+++ src/api/api.go
@@ -68,6 +68,7 @@ func RegisterRoutes(r *gin.Engine, cfg *config.Config) {
 		carModelColors := v1.Group("/car-model-colors", middlewares.Authentication(cfg), middlewares.Authorization([]string{"admin"}))
 		carModelYears := v1.Group("/car-model-years", middlewares.Authentication(cfg), middlewares.Authorization([]string{"admin"}))
 		carModelPriceHistories := v1.Group("/car-model-price-histories", middlewares.Authentication(cfg), middlewares.Authorization([]string{"admin"}))
+		carModelImages := v1.Group("/car-model-images", middlewares.Authentication(cfg), middlewares.Authorization([]string{"admin"}))
 
 		// Test
 		routers.Health(health)
@@ -95,6 +96,9 @@ func RegisterRoutes(r *gin.Engine, cfg *config.Config) {
 		routers.CarModelColor(carModelColors, cfg)
 		routers.CarModelYear(carModelYears, cfg)
 		routers.CarModelPriceHistory(carModelPriceHistories, cfg)
+		routers.CarModelImage(carModelImages, cfg)
+
+		r.Static("/static", "./uploads")
 	}
 
 	v2 := api.Group("/v2")
