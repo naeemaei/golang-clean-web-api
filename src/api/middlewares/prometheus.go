afbf48f4bdd7f67274a711c64e4147b5e6269304
hamed <h.naimaei@gmail.com>
2023-03-16 18:47:11 +0330

    Add status code metric to request duration metric

--- src/api/middlewares/prometheus.go
+++ src/api/middlewares/prometheus.go
@@ -1,19 +1,22 @@
 package middlewares
 
 import (
+	"strconv"
+	"time"
+
 	"github.com/gin-gonic/gin"
 	"github.com/naeemaei/golang-clean-web-api/pkg/metrics"
-	"github.com/prometheus/client_golang/prometheus"
 )
 
 func Prometheus() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		start := time.Now()
 		path := c.FullPath()
 		method := c.Request.Method
-		timer := prometheus.NewTimer(metrics.HttpDuration.WithLabelValues(path, method))
 
-		c.Next() 
-		
-		timer.ObserveDuration()
+		c.Next()
+		status := c.Writer.Status()
+		metrics.HttpDuration.WithLabelValues(path, method, strconv.Itoa(status)).
+			Observe(float64(time.Since(start) / time.Millisecond))
 	}
 }
