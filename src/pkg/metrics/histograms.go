ce8e10660adb00826cdbd62eccecaae7bdc44a39
Hamed Naeemaei <h.naimaei@gmail.com>
2023-03-17 11:45:25 +0330

    Add buckets of histogram metrics

--- src/pkg/metrics/histograms.go
+++ src/pkg/metrics/histograms.go
@@ -2,9 +2,9 @@ package metrics
 
 import "github.com/prometheus/client_golang/prometheus"
 
-
 var HttpDuration = prometheus.NewHistogramVec(
 	prometheus.HistogramOpts{
-		Name: "http_response_time",
-		Help: "Duration of HTTP requests",
-}, []string{"path","method","status_code"})
+		Name:    "http_response_time",
+		Help:    "Duration of HTTP requests",
+		Buckets: []float64{1, 2, 5, 10, 50, 100, 200, 500, 1000, 2000, 5000, 10000},
+	}, []string{"path", "method", "status_code"})
